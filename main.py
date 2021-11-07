3fbf1e1c08d59e263715f05fb843c85a928fb4a5
Lewin <lewinkoon+github@gmail.com>
2021-11-07 15:45:43 +0100

    Update add site

--- main.py
+++ main.py
@@ -23,25 +23,25 @@ def main():
     print("1. Read the hosts file")
     print("2. Add new blocked site")
     print("3. Remove blocked site")
-
     print("0. Exit\n")
+
     while choice == '0':
 
         choice = input("What would you like to do?: ")
 
         # Declare variable lines
         sites = []
-        with open("hosts", "r") as file:
-            raw = file.readlines()
 
         if choice == "1":
+            with open("hosts", "r") as file:
+                raw = file.readlines()
             print("\nHere is the list of blocked sites\n")
             for i, str in enumerate(raw[::2]):
                 web = re.split(r'\t+', str.rstrip('\n'))[1]
                 sites.append(web)
                 print(i, '\t' + web)
         elif choice == "2":
-            print("\nType the site addresss you want to block.\n")
+            add_site()
         elif choice == "3":
             print("\nType the blocked site address you want to remove\n")
         elif choice == "0":
@@ -49,7 +49,15 @@ def main():
             quit()
         else:
             print("\nPlease enter a valid number.\n")
-        choice = "0"
+            choice = '0'
+
+
+def add_site():
+    while True:
+        inp = input("\nPlease, type the site addresss you want to block: ")
+        if re.match('[a-zA-Z0-9]{1,256}\.[a-z]{1,6}$', inp):
+            return inp
+        print('\nInvalid format, please enter again: ')
 
 
 # # Execution time measurement
